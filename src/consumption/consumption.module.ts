d546aff297b799a22f748c4cdb3ab58dd0062701
vihribeiro <133062998+vihribeiro@users.noreply.github.com>
2024-11-04 15:53:52 -0300

    update

--- src/consumption/consumption.module.ts
+++ src/consumption/consumption.module.ts
@@ -1,9 +1,13 @@
+// src/consumption/consumption.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConsumptionService } from './consumption.service';
 import { ConsumptionController } from './consumption.controller';
+import { Consumption } from './consumption.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Consumption])],
   providers: [ConsumptionService],
-  controllers: [ConsumptionController]
+  controllers: [ConsumptionController],
 })
 export class ConsumptionModule {}
